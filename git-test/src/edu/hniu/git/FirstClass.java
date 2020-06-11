c389b2a7157d8378501667714b74025a4cfc1bc5
ChenAnStar <2558147190@qq.com>
2020-06-11 14:04:52 +0800

    修改1

--- git-test/src/edu/hniu/git/FirstClass.java
+++ git-test/src/edu/hniu/git/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
     public static void main(String[] args) {
         //修改
         System.out.println("修改");
+        System.out.println("修改");
         System.out.println("git");
     }
 }
